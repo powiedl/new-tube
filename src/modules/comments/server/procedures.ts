60ff870f54b4ebe237f6959e3ffc30b6b117825c
powiedl <rolandpi@gmx.at>
2025-02-16 16:05:46 +0100

    video2 at 3:30

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -6,7 +6,7 @@ import {
 } from '@/trpc/init';
 import { db } from '@/db';
 import { comments, users } from '@/db/schema';
-import { eq, getTableColumns } from 'drizzle-orm';
+import { and, count, desc, eq, getTableColumns, lt, or } from 'drizzle-orm';
 
 export const commentsRouter = createTRPCRouter({
   create: protectedProcedure
@@ -31,19 +31,61 @@ export const commentsRouter = createTRPCRouter({
     .input(
       z.object({
         videoId: z.string().uuid(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date(),
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100),
       })
     )
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, cursor, limit } = input;
+      const [[totalData], data] = await Promise.all([
+        // Promise.all returns an array with the resolved Promise for each element in the parameter array
+        // And as the first query only has one element, but still is an array, we need a second pair of [] to only get this first element of totalData
+        db
+          .select({ count: count() })
+          .from(comments)
+          .where(eq(comments.videoId, videoId)),
+        db
+          .select({
+            ...getTableColumns(comments),
+            user: users,
+            //totalCount: db.$count(comments, eq(comments.videoId, videoId)), // technical correct, but in this way the total number of comments gets an attribute to every comment!
+          })
+          .from(comments)
+          .where(
+            and(
+              eq(comments.videoId, videoId),
+              cursor
+                ? or(
+                    lt(comments.updatedAt, cursor.updatedAt),
+                    and(
+                      eq(comments.updatedAt, cursor.updatedAt),
+                      lt(comments.id, cursor.id)
+                    )
+                  )
+                : undefined
+            )
+          )
+          .innerJoin(users, eq(comments.userId, users.id))
+          .orderBy(desc(comments.updatedAt), desc(comments.id))
+          .limit(limit + 1),
+      ]);
 
-      const data = await db
-        .select({
-          ...getTableColumns(comments),
-          user: users,
-        })
-        .from(comments)
-        .where(eq(comments.videoId, videoId))
-        .innerJoin(users, eq(comments.userId, users.id));
-      return data;
+      const hasMore = data.length > limit;
+      // Remove the last item if there is more data
+
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      // Set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? { id: lastItem.id, updatedAt: lastItem.updatedAt }
+        : null;
+
+      return { totalCount: totalData.count, items, nextCursor };
     }),
 });
