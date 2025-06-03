69ecf8ec1ad10fb50a7a7fcfb490d6bff2135737
powiedl <rolandpi@gmx.at>
2025-06-03 06:58:54 +0200

    changed the Gemini AI models, because the used ones will be turned of in Sept 2025

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,5 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_APIKEY!);
 
-export const GEMINI_PREFERED_MODEL = 'gemini-2.0-flash-lite-preview-02-05';
-export const GEMINI_BACKUP_MODEL = 'gemini-1.5-flash';
+export const GEMINI_PREFERED_MODEL = 'gemini-2.5-flash-preview-05-20';
+export const GEMINI_BACKUP_MODEL = 'gemini-2.0-flash-lite';
