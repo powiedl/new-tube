031b0e7a6de93cb1e8bbb4506994da08dfdf894a
powiedl <rolandpi@gmx.at>
2025-11-30 05:03:51 +0100

    added environment variables for GEMINI models (at the moment only GEMINI_PREFERED_MODEL is really used, there is no fallback implemented)

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,5 +1,7 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_APIKEY!);
 
-export const GEMINI_PREFERED_MODEL = 'gemini-2.5-flash-preview-05-20';
-export const GEMINI_BACKUP_MODEL = 'gemini-2.0-flash-lite';
+export const GEMINI_PREFERED_MODEL =
+  process.env.GEMINI_PREFERED_MODEL || 'gemini-2.5-flash-preview-09-2025';
+export const GEMINI_BACKUP_MODEL =
+  process.env.GEMINI_BACKUP_MODEL || 'gemini-2.5-flash';
